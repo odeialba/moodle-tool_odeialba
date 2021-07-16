e864cf7ca90d29d0af07d525df22c67365ff34ae
Odei Alba <odeialba@gmail.com>
2021-07-16 12:53:57 +0200

    Change release version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_odeialba';
-$plugin->version = 2021071501;
+$plugin->version = 2021071600;
 $plugin->requires = 2021070100;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '1.3.1';
+$plugin->release = '1.4';
