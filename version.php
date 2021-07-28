d9a9768cb2310e5dd884399eef7d5a8669857848
Odei Alba <odeialba@gmail.com>
2021-07-28 11:07:45 +0200

    Change minimum required to make travis pass

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_odeialba';
 $plugin->version = 2021072800;
-$plugin->requires = 2021070100;
+$plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '1.9';
